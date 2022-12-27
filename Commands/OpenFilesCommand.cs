35de8df7116b0e8d5391f7fe5d1f258a5d2fcb41
Ricky Stam <k.stam@hotmail.com>
2022-12-27 20:40:24 +0200

    Add project files.

---
+++ Commands/OpenFilesCommand.cs
@@ -0,0 +1,90 @@
+﻿using PRFileOpener.Options;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace PRFileOpener
+{
+    [Command(PackageIds.OpenFilesCommand)]
+    internal sealed class OpenFilesCommand : BaseCommand<OpenFilesCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var currentSolution = await VS.Solutions.GetCurrentSolutionAsync();
+            var currentSolutionPath = Directory.GetParent(currentSolution.FullPath).FullName;
+
+            var targetBranchName = OptionsPageDialog.Instance.TargetBranchName;
+            if (targetBranchName is null) return;
+
+            var branchGetter = new BranchGetter();
+            var currentBranchName = branchGetter.GetCurrentBranchName(currentSolutionPath);
+
+            var filesChanged = new List<string>();
+            string stdError = null;
+            var nextIsFiles = false;
+
+            using (var cmdProcess = new Process())
+            {
+                cmdProcess.StartInfo = new ProcessStartInfo()
+                {
+                    WorkingDirectory = currentSolutionPath,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                };
+
+                cmdProcess.Start();
+                cmdProcess.OutputDataReceived += (s, ev) =>
+                {
+                    if (nextIsFiles)
+                    {
+                        if (!string.IsNullOrEmpty(ev.Data))
+                        {
+                            filesChanged.Add(ev.Data);
+                        }
+                        else
+                        {
+                            nextIsFiles = false;
+                        }
+
+                    }
+                    if (ev.Data != null && ev.Data.Contains("git diff --name-only"))
+                    {
+                        nextIsFiles = true;
+                    }
+                };
+                cmdProcess.ErrorDataReceived += (s, ev) => { stdError += ev.Data + Environment.NewLine; };
+                cmdProcess.BeginOutputReadLine();
+                cmdProcess.BeginErrorReadLine();
+
+                using (var sw = cmdProcess.StandardInput)
+                {
+                    await sw.WriteLineAsync("git pull -q");
+                    await sw.WriteLineAsync($"git fetch origin {targetBranchName}:{targetBranchName}");
+                    await sw.WriteLineAsync($"git diff --name-only {currentBranchName} {targetBranchName}");
+                }
+                cmdProcess.WaitForExit();
+            };
+
+            if (!string.IsNullOrWhiteSpace(stdError))
+            {
+                await VS.MessageBox.ShowErrorAsync("Error", stdError);
+            }
+
+            var sp = new Stopwatch();
+            sp.Start();
+            foreach (var file in filesChanged)
+            {
+                var filePath = $"{currentSolutionPath}\\{file.Replace("/", "\\")}";
+                await VS.Documents.OpenAsync(filePath);
+            }
+
+            sp.Stop();
+            Debug.WriteLine($"Elapsed ms was : {sp.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ Options/Options/OptionsPageDialog.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace PRFileOpener.Options
+{
+    internal partial class OptionsProvider
+    {
+        // Register the options with this attribute on your package class:
+        // [ProvideOptionPage(typeof(OptionsProvider.OptionsPageDialogOptions), "PRFileOpener.Options", "OptionsPageDialog", 0, 0, true, SupportsProfiles = true)]
+        [ComVisible(true)]
+        public class OptionsPageDialogOptions : BaseOptionPage<OptionsPageDialog> { }
+    }
+
+    public class OptionsPageDialog : BaseOptionModel<OptionsPageDialog>
+    {
+        [Category("PR File Opener")]
+        [DisplayName("Target branch name")]
+        [Description("Target branch name")]
+        public string TargetBranchName { get; set; } = "main";
+    }
+}

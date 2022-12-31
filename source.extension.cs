3354045a665de8bda945181471fb973e7540ecc5
Ricky Stam <k.stam@hotmail.com>
2022-12-31 13:18:52 +0200

    update name

--- source.extension.cs
+++ source.extension.cs
@@ -7,8 +7,8 @@ namespace PRFileOpener
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "PRFileOpener.6b8d6338-e27f-471b-be74-c11619f176b9";
-        public const string Name = "PRFileOpener";
+        public const string Id = "PRFileOpener.6b8d6338-e27f-471b-be74-c11619f176c9";
+        public const string Name = "PR File Opener";
         public const string Description = @"VS Extension to help you do PR reviews easier and faster by opening all added/modified files of current branch in comparison with PRs target branch.";
         public const string Language = "en-US";
         public const string Version = "0.1";
